ca118e5e97f5c83c34e22010363342254727c363
varesc <varesc@gmail.com>
2018-05-28 20:55:39 +0700

    add delete todoById Route

--- server/server.js
+++ server/server.js
@@ -59,6 +59,24 @@ app.get('/todos/:id',(req,res)=>{
 
 });
 
+app.delete('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+       return res.status(400).send('invalid Id');
+    }
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if(todo){
+           return res.status(200).send(todo);
+        }
+
+        return res.status(404).send("Could not find your Todo");
+    });
+},(e)=>{
+    res.status(400).send('something wrong with the connection');
+}).catch((e)=>{
+    res.status(400).send();
+});
+
 
 
 app.listen(port,()=>{
