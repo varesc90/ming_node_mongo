ddbc684891448b10755fbfd8c65b940d59bf6f9b
varesc <varesc@gmail.com>
2018-05-31 23:05:39 +0700

    Add postuser route and token methods

--- server/server.js
+++ server/server.js
@@ -111,9 +111,12 @@ app.post('/users',(req,res)=>{
     var user = new User(body);
 
     user.save().then((user)=>{
-        res.send(user);
+        return user.generateAuthToken();
+
     },(err)=>{
         res.status(400).send(err);
+    }).then((token)=>{
+        res.header('x-auth',token).send(user);
     });
 });
 
