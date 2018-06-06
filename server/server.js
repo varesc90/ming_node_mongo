a67a3fbc666b121e23b6015e688e4f9119874ec8
varesc <varesc@gmail.com>
2018-06-06 21:14:57 +0700

    add delete route

--- server/server.js
+++ server/server.js
@@ -142,6 +142,16 @@ app.post('/users/login',(req,res)=>{
     });
 });
 
+app.delete('/users/me/token',authenticate,(req,res)=>{
+    req.user.removeToken(req.token).then(()=>{
+        res.status(200).send();
+    }).catch((e)=>{
+        res.send(e);
+    });
+});
+
+
+
 app.listen(port,()=>{
     console.log(`Started on port ${port}`);
 });
